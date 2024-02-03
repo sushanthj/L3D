7fa19460b28da7eec34f043703a931b14c98e581
sushanth <sushanth.jayanth@gmail.com>
2024-02-03 14:13:11 -0500

    few updates

--- HW1/starter/camera_transforms.py
+++ HW1/starter/camera_transforms.py
@@ -10,6 +10,7 @@ import torch
 import numpy as np
 
 from starter.utils import get_device, get_mesh_renderer
+from pytorch3d.vis.plotly_vis import plot_scene
 
 
 def render_cow(
@@ -21,7 +22,27 @@ def render_cow(
 ):
     if device is None:
         device = get_device()
+    # NOTE: Notice how io.load_objs_as_meshes is different than creating our own mesh using
+    #       pytorch3d.structures.Meshes
     meshes = pytorch3d.io.load_objs_as_meshes([cow_path]).to(device)
+
+    # Redfining R_relative and T_relative according to requirements
+    ######## 1. 90 degree counterclock rotation of camera about z ################
+    # R_relative = [[0, -1, 0], [1, 0, 0], [0, 0, 1]]
+    # T_relative = [0, 0, 0]
+
+    ######## 2. 90 degree counterclock rotation of camera about y ################
+    # R_relative = [[0, 0, -1], [0, 1, 0], [1, 0, 0]]
+    # T_relative = [3, 0, 3] ###! ASK HOW?
+
+    ######## 3. Move camera away from the cow ################
+    # R_relative = [[1, 0, 0], [0, 1, 0], [0, 0, 1]]
+    # T_relative = [0, 0, 3]
+
+    ######## 4. Move camera sideways from the cow and lift up ################
+    # theta = np.radians(0)
+    # R_relative = [[1, 0, 0], [0, np.cos(-theta), -np.sin(-theta)], [0, np.sin(-theta), np.cos(-theta)]]
+    # T_relative = [0.5, -0.5, 0]
 
     R_relative = torch.tensor(R_relative).float()
     T_relative = torch.tensor(T_relative).float()
@@ -35,6 +56,15 @@ def render_cow(
     )
     lights = pytorch3d.renderer.PointLights(location=[[0, 0.0, -3.0]], device=device,)
     rend = renderer(meshes, cameras=cameras, lights=lights)
+
+    fig = plot_scene({
+        "figure": {
+            "Mesh": meshes,
+            "Camera": cameras,
+        }
+    })
+    fig.show()
+
     return rend[0, ..., :3].cpu().numpy()
 
 
@@ -42,7 +72,7 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--cow_path", type=str, default="data/cow_with_axis.obj")
     parser.add_argument("--image_size", type=int, default=256)
-    parser.add_argument("--output_path", type=str, default="images/transform_cow.jpg")
+    parser.add_argument("--output_path", type=str, default="images/transform_cow.png")
     args = parser.parse_args()
 
     plt.imsave(args.output_path, render_cow(cow_path=args.cow_path, image_size=args.image_size))
