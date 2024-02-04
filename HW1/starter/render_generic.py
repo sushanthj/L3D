361ce7df1652d875844f40dc01b0e420355bd3a0
sushanth <sushanth.jayanth@gmail.com>
2024-02-04 11:40:03 -0500

    added runtime numbers

--- HW1/starter/render_generic.py
+++ HW1/starter/render_generic.py
@@ -15,6 +15,8 @@ import numpy as np
 import pytorch3d
 import torch
 import imageio
+import time
+import sys
 
 from starter.utils import get_device, get_mesh_renderer, get_points_renderer, unproject_depth_image
 
@@ -239,6 +241,7 @@ def render_torus(image_size=256, num_samples=200, device=None):
     """
     Renders a Torus
     """
+    start = time.time()
     background_color=(1, 1, 1)
     if device is None:
         device = get_device()
@@ -261,8 +264,9 @@ def render_torus(image_size=256, num_samples=200, device=None):
     color = color.unsqueeze(0).expand(num_views, -1, -1)
 
     torus_point_cloud = pytorch3d.structures.Pointclouds(
-        points=points, features=color,
-    ).to(device)
+        points=points, features=color,)
+    print("Memory occupied by torus_point_cloud in MB: ", sys.getsizeof(torus_point_cloud) / (1024 * 1024))
+    torus_point_cloud = torus_point_cloud.to(device)
 
     R, T = pytorch3d.renderer.look_at_view_transform(
         dist=6,
@@ -287,10 +291,17 @@ def render_torus(image_size=256, num_samples=200, device=None):
     )
 
     images = renderer(torus_point_cloud, cameras=many_cameras)
+    end = time.time()
+    print("Time taken to render parametric torus: ", end - start)
     images = images.cpu().numpy()
     img_list = [np.clip((img.squeeze() * 255).astype(np.uint8), 0, 255)[:,:,0:3] for img in images]
     imageio.mimsave('images/torus_pointcloud_360.gif', img_list, loop=10, duration = 0.05)
     return img_list[0]
+
+    """
+    Memory occupied by torus_point_cloud in MB:  4.57763671875e-05
+    Time taken to render parametric torus:  0.31734442710876465
+    """
 
 
 def render_custom_shape(image_size=256, num_samples=200, device=None):
@@ -373,15 +384,144 @@ def render_sphere_mesh(image_size=256, voxel_size=64, device=None):
     textures = (vertices - vertices.min()) / (vertices.max() - vertices.min())
     textures = pytorch3d.renderer.TexturesVertex(vertices.unsqueeze(0))
 
-    mesh = pytorch3d.structures.Meshes([vertices], [faces], textures=textures).to(
-        device
-    )
+    mesh = pytorch3d.structures.Meshes([vertices], [faces], textures=textures).to(device)
     lights = pytorch3d.renderer.PointLights(location=[[0, 0.0, -4.0]], device=device,)
     renderer = get_mesh_renderer(image_size=image_size, device=device)
     R, T = pytorch3d.renderer.look_at_view_transform(dist=3, elev=0, azim=180)
     cameras = pytorch3d.renderer.FoVPerspectiveCameras(R=R, T=T, device=device)
     rend = renderer(mesh, cameras=cameras, lights=lights)
     return rend[0, ..., :3].detach().cpu().numpy().clip(0, 1)
+
+
+def render_torus_implicit(image_size=256, voxel_size=64, device=None):
+    start = time.time()
+    if device is None:
+        device = get_device()
+    min_value = -1.1
+    max_value = 1.1
+    R = 0.6
+    r = 0.4
+    X, Y, Z = torch.meshgrid([torch.linspace(min_value, max_value, voxel_size)] * 3)
+    voxels = (torch.sqrt(X ** 2 + Y ** 2) - R) ** 2 + Z ** 2 - r ** 2
+    vertices, faces = mcubes.marching_cubes(mcubes.smooth(voxels), isovalue=0)
+    vertices = torch.tensor(vertices).float()
+    faces = torch.tensor(faces.astype(int))
+    # Vertex coordinates are indexed by array position, so we need to
+    # renormalize the coordinate system.
+    vertices = (vertices / voxel_size) * (max_value - min_value) + min_value
+    textures = (vertices - vertices.min()) / (vertices.max() - vertices.min())
+
+    num_views = 12
+    vertices = vertices.unsqueeze(0).expand(num_views, -1, -1)
+    faces = faces.unsqueeze(0).expand(num_views, -1, -1)
+    textures = textures.unsqueeze(0).expand(num_views, -1, -1)
+
+    textures = pytorch3d.renderer.TexturesVertex(vertices)
+
+    mesh = pytorch3d.structures.Meshes(vertices, faces, textures=textures)
+    print("Memory occupied by mesh in MB: ", sys.getsizeof(mesh) / (1024 * 1024))
+    mesh = mesh.to(device)
+    lights = pytorch3d.renderer.PointLights(location=[[0, 0.0, -4.0]], device=device,)
+    renderer = get_mesh_renderer(image_size=image_size, device=device)
+    # R, T = pytorch3d.renderer.look_at_view_transform(dist=3, elev=0, azim=180)
+    # cameras = pytorch3d.renderer.FoVPerspectiveCameras(R=R, T=T, device=device)
+
+    R, T = pytorch3d.renderer.look_at_view_transform(
+        dist=6,
+        elev=0,
+        azim=np.linspace(-180, 180, num_views, endpoint=False),
+    )
+
+    # Additional rotation of 10 degrees about the x-axis
+    additional_rotation = torch.tensor([[1,  0, 0],
+                                        [0, np.cos(np.radians(20)), np.sin(np.radians(20))],
+                                        [0,  -np.sin(np.radians(20)), np.cos(np.radians(20))]],
+                                        dtype=R.dtype, device=R.device)
+    additional_rotation = additional_rotation.unsqueeze(0).expand(num_views, -1, -1)
+
+    # Apply additional rotation to the original rotations
+    R_additional = R @ additional_rotation
+
+    many_cameras = pytorch3d.renderer.FoVPerspectiveCameras(
+        R=R_additional,
+        T=T,
+        device=device
+    )
+
+    images = renderer(mesh, cameras=many_cameras, lights=lights)
+    end = time.time()
+    print("Time taken to render implicit torus: ", end - start)
+    images = images.cpu().numpy()
+    img_list = [np.clip((img.squeeze() * 255).astype(np.uint8), 0, 255)[:,:,0:3] for img in images]
+    imageio.mimsave('images/implicit_torus_360.gif', img_list, loop=10, duration = 0.05)
+    return img_list[0]
+
+    """
+    Memory occupied by mesh in MB:  4.57763671875e-05
+    Time taken to render implicit torus:  2.4901247024536133
+    """
+
+
+def render_custom_implicit(image_size=256, voxel_size=64, device=None):
+    if device is None:
+        device = get_device()
+    min_value = -1.1
+    max_value = 1.1
+    a = 0.6
+    b = 0.6
+    c = 0.8
+    X, Y, Z = torch.meshgrid([torch.linspace(min_value, max_value, voxel_size)] * 3)
+    voxels = X ** 2 / a ** 2 + Y ** 2 / b ** 2 - Z ** 2 / c ** 2 - 1
+    vertices, faces = mcubes.marching_cubes(mcubes.smooth(voxels), isovalue=0)
+    vertices = torch.tensor(vertices).float()
+    faces = torch.tensor(faces.astype(int))
+    # Vertex coordinates are indexed by array position, so we need to
+    # renormalize the coordinate system.
+    vertices = (vertices / voxel_size) * (max_value - min_value) + min_value
+    textures = (vertices - vertices.min()) / (vertices.max() - vertices.min())
+
+    num_views = 12
+    vertices = vertices.unsqueeze(0).expand(num_views, -1, -1)
+    faces = faces.unsqueeze(0).expand(num_views, -1, -1)
+    textures = textures.unsqueeze(0).expand(num_views, -1, -1)
+
+    textures = pytorch3d.renderer.TexturesVertex(vertices)
+
+    mesh = pytorch3d.structures.Meshes(vertices, faces, textures=textures).to(
+        device
+    )
+    lights = pytorch3d.renderer.PointLights(location=[[0, 0.0, -4.0]], device=device,)
+    renderer = get_mesh_renderer(image_size=image_size, device=device)
+    # R, T = pytorch3d.renderer.look_at_view_transform(dist=3, elev=0, azim=180)
+    # cameras = pytorch3d.renderer.FoVPerspectiveCameras(R=R, T=T, device=device)
+
+    R, T = pytorch3d.renderer.look_at_view_transform(
+        dist=6,
+        elev=0,
+        azim=np.linspace(-180, 180, num_views, endpoint=False),
+    )
+
+    # Additional rotation of 10 degrees about the x-axis
+    additional_rotation = torch.tensor([[1,  0, 0],
+                                        [0, np.cos(np.radians(20)), np.sin(np.radians(20))],
+                                        [0,  -np.sin(np.radians(20)), np.cos(np.radians(20))]],
+                                        dtype=R.dtype, device=R.device)
+    additional_rotation = additional_rotation.unsqueeze(0).expand(num_views, -1, -1)
+
+    # Apply additional rotation to the original rotations
+    R_additional = R @ additional_rotation
+
+    many_cameras = pytorch3d.renderer.FoVPerspectiveCameras(
+        R=R_additional,
+        T=T,
+        device=device
+    )
+
+    images = renderer(mesh, cameras=many_cameras, lights=lights)
+    images = images.cpu().numpy()
+    img_list = [np.clip((img.squeeze() * 255).astype(np.uint8), 0, 255)[:,:,0:3] for img in images]
+    imageio.mimsave('images/implicit_hyperbolloid_360.gif', img_list, loop=10, duration = 0.05)
+    return img_list[0]
 
 
 if __name__ == "__main__":
@@ -391,7 +531,8 @@ if __name__ == "__main__":
         type=str,
         default="point_cloud",
         choices=["point_cloud", "point_cloud_first", "point_cloud_second",
-                 "point_cloud_composite", "torus", "custom_shape", "parametric", "implicit"],
+                 "point_cloud_composite", "torus", "custom_shape", "parametric",
+                 "implicit", "torus_implicit", "custom_implicit"],
     )
     parser.add_argument("--output_path", type=str, default="images/bridge.jpg")
     parser.add_argument("--image_size", type=int, default=256)
@@ -413,6 +554,10 @@ if __name__ == "__main__":
         image = render_custom_shape(image_size=args.image_size, num_samples=args.num_samples)
     elif args.render == "implicit":
         image = render_sphere_mesh(image_size=args.image_size)
+    elif args.render == "torus_implicit":
+        image = render_torus_implicit(image_size=args.image_size)
+    elif args.render == "custom_implicit":
+        image = render_custom_implicit(image_size=args.image_size)
     else:
         raise Exception("Did not understand {}".format(args.render))
     plt.imsave(args.output_path, image)
